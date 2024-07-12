bb86e171ab0f2f747534c17255fd930dcce6e70a
mohamedali392001 <ly8197884@gmail.com>
2024-07-11 19:53:10 -0700

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constants.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/model/note_model.dart';
+import 'package:note_app/simple_bloc_observer.dart';
 import 'package:note_app/view/note_view.dart';
 
 void main() async{
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
   Hive.registerAdapter(NoteModelAdapter());
