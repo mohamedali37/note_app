41313f551cd40633d9f1ad36b4f75ded2db7b90d
mohamedali392001 <ly8197884@gmail.com>
2024-07-11 19:44:19 -0700

    add add note cubit to bottom sheet

--- lib/widget/note_bottom_sheet.dart
+++ lib/widget/note_bottom_sheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_states.dart';
 import 'package:note_app/widget/custom_button.dart';
 import 'package:note_app/widget/custom_text_field.dart';
 
@@ -19,48 +23,63 @@ class _NoteBottomSheetState extends State<NoteBottomSheet> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          autovalidateMode: autoValidateMode,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 35,
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+            else if (state is AddNoteFailure) {
+              print('edweve');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: Form(
+                key: formKey,
+                autovalidateMode: autoValidateMode,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 35,
+                    ),
+                    CustomTextField(
+                      onSaved: (value) {
+                        title = value;
+                      },
+                      text: 'title',
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomTextField(
+                      onSaved: (value) {
+                        subTitle = value;
+                      },
+                      text: 'content',
+                      maxLines: 5,
+                    ),
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    CustomButton(
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+                        } else {
+                          autoValidateMode = AutovalidateMode.always;
+                          setState(() {});
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                  ],
+                ),
               ),
-              CustomTextField(
-                onSaved: (value) {
-                  title = value;
-                },
-                text: 'title',
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              CustomTextField(
-                onSaved: (value) {
-                  subTitle = value;
-                },
-                text: 'content',
-                maxLines: 5,
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              CustomButton(
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                  } else {
-                    autoValidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
