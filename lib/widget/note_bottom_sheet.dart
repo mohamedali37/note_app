0c2b23f77da817d12f0d5874e91ced8979d7c1cb
mohamedali392001 <ly8197884@gmail.com>
2024-07-11 20:23:56 -0700

    edit bottom sheet

--- lib/widget/note_bottom_sheet.dart
+++ lib/widget/note_bottom_sheet.dart
@@ -3,19 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_states.dart';
-import 'package:note_app/model/note_model.dart';
-import 'package:note_app/widget/custom_button.dart';
-import 'package:note_app/widget/custom_text_field.dart';
+import 'package:note_app/widget/note_form.dart';
 
-class NoteBottomSheet extends StatefulWidget {
+class NoteBottomSheet extends StatelessWidget {
   const NoteBottomSheet({super.key});
 
-  @override
-  State<NoteBottomSheet> createState() => _NoteBottomSheetState();
-}
-
-class _NoteBottomSheetState extends State<NoteBottomSheet> {
-  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -32,8 +24,8 @@ class _NoteBottomSheetState extends State<NoteBottomSheet> {
         builder: (context, state) {
           return ModalProgressHUD(
             inAsyncCall: state is AddNoteLoading ? true : false,
-            child: SingleChildScrollView(
-              child: 
+            child: const SingleChildScrollView(
+              child: NoteForm()
             ),
           );
         },
