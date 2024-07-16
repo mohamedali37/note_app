397d79ac34d9f54e26ab4c56353b8e0cb1011f3c
mohamedali392001 <ly8197884@gmail.com>
2024-07-16 16:24:16 -0700

    trigger note body

--- lib/widget/note_body.dart
+++ lib/widget/note_body.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/note_item_cubit/note_item_cubit.dart';
 import 'package:note_app/widget/custom_app_bar.dart';
 import 'package:note_app/widget/note_list_view.dart';
 
-class NoteBody extends StatelessWidget {
+class NoteBody extends StatefulWidget {
   const NoteBody({super.key});
 
   @override
+  State<NoteBody> createState() => _NoteBodyState();
+}
+
+class _NoteBodyState extends State<NoteBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NoteCubit>(context).feachAllNotes();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
